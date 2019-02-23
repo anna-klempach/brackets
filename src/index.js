6482d37e9f20a7b09d83465d8bbc00a1c1612cee
anna-klempach <klempach.teaching@gmail.com>
2019-02-23 20:19:45 +0300

    Update README.md

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+
+  let strArray = str.split('');
+  let flag = 0;
+  
+  if (strArray.length % 2 !== 0) {
+  	return false;
+  }
+
+  while (true) {
+
+  	flag = 0;
+
+  	for (var i = 0; i < bracketsConfig.length; i++) {
+
+  		for (let right = strArray.length - 1; right >= 0; right--) {
+
+	  		if ( strArray[right] == bracketsConfig[i][1] && strArray[right-1] == bracketsConfig[i][0]) {
+	  			strArray.splice(right-1, 2);
+	  			flag = 1;
+	  		}
+
+	  		if ( strArray.length === 0) {
+	  			return true;
+	  		}
+  		}
+  	}
+  	
+  	if(flag === 0) {
+  		return false;
+  	}  	
+  }
 }
